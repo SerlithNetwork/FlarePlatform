2de6cda85dec6a3de122ddceb5a35ddc9db9448e
Toffikk <slodkitofik@gmail.com>
2025-10-25 20:39:43 +0200

    feat: multiplatform jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,20 @@
 plugins {
     java
+    id("com.gradleup.shadow") version "9.2.2"
 }
+
 group = "co.technove"
 version = "2.0.0"
 
 val flareVersion = providers.gradleProperty("flareVersion").get()
 val oshiVersion = providers.gradleProperty("oshiVersion").get()
 
-subprojects {
+dependencies {
+    implementation(project(":paper"))
+    implementation(project(":velocity"))
+}
+
+allprojects {
     apply(plugin = "java")
 
     group = rootProject.group
@@ -30,5 +37,15 @@ subprojects {
             options.encoding = Charsets.UTF_8.name()
             options.release.set(21)
         }
+    }
+}
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        archiveClassifier.set("")
+        dependsOn(":velocity:shadowJar")
+        dependsOn(":paper:shadowJar")
     }
 }
