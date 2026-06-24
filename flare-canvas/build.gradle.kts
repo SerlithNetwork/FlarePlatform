460d479a7296e442e849061c8f2b0cebaa9a9858
Toffikk <slodkitofik@gmail.com>
2026-06-24 23:28:19 +0200

    use canvas releases maven

--- flare-canvas/build.gradle.kts
+++ flare-canvas/build.gradle.kts
@@ -1,10 +1,9 @@
 repositories {
-    maven("https://maven.canvasmc.io/snapshots") {
-        name = "canvas-snapshots"
-    }
+    maven("https://maven.canvasmc.io/releases")
 }
 
 dependencies {
     compileOnly(projects.flareCommon)
+    // compileOnly(libs.canvasApi)
     compileOnly(files("libs/canvas-api-26.2.local-SNAPSHOT.jar"))
 }
