f3368f2b2ca8472b6d14c25abb2813cecdb798c5
Biquaternions <biquaternions@serlith.net>
2026-06-27 16:30:35 -0500

    Add adventure to fish module

--- flare-fish/build.gradle.kts
+++ flare-fish/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     compileOnly(projects.flareCommon)
     compileOnly(files("libs/fish-api-1.21.11-R0.1-SNAPSHOT.jar"))
+    compileOnly("net.kyori:adventure-api:4.26.1")
     compileOnly("org.jetbrains:annotations:26.1.0")
 }
